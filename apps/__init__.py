08271c65f4de4a8bc6a1b276e8d1ca1119883e7a
shubham <shubham@kifwatindia.com>
2024-05-28 12:19:53 +0530

    First commit

---
+++ apps/__init__.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+import os
+import click
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+
+
+def register_blueprints(app):
+    for module_name in ('authentication', 'home'):
+        module = import_module('apps.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        try:
+            db.create_all()
+        except Exception as e:
+
+            print('> Error: DBMS Exception: ' + str(e) )
+
+            # fallback to SQLite
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+
+            print('> Fallback to SQLite ')
+            db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove() 
+
+def register_cli_commands(app):
+    @app.cli.command("create-admin")
+    @click.argument("username")
+    @click.argument("email")
+    @click.argument("password")
+    def create_admin_user(username, email, password):
+        """Create a new admin user with the given username, email, password."""
+        from apps.authentication.models import Users
+        from apps.authentication.util import hash_pass
+
+        # hashed_password = hash_pass(password)
+        new_user = Users(username=username, email=email, password=password, role="admin")
+        db.session.add(new_user)
+        db.session.commit()
+        click.echo(f"Admin user with username: {username} created successfully.")
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    register_extensions(app)
+    register_blueprints(app)
+    configure_database(app)
+    register_cli_commands(app)
+    return app
